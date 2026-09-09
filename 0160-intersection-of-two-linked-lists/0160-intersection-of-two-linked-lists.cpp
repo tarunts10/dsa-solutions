93712fdc7556189dbe70861fccf0b171511732a7
Tarun TS <taruntejaswishetty@gmail.com>
2026-09-09 20:15:36 +0530

    Time: 51 ms (83.24%), Space: 24 MB (24.97%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,8 +8,19 @@
  */
 class Solution {
 public:
+    /*ListNode *collisionPoint(ListNode *t1,ListNode *t2,int d){
+        while(d){
+            d--;
+            t1=t1->next;
+        }
+        while(t1!=t2){
+            t1=t1->next;
+            t2=t2->next;
+        }
+        return t1;
+    }*/
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* temp1=headA;
+        /*ListNode* temp1=headA;
         ListNode* temp2=headB;
         unordered_map<ListNode*,int> mpp;
 
@@ -24,6 +35,43 @@ public:
             }
             temp2=temp2->next;
         }
-        return NULL;
+        return NULL;*/
+
+        /*ListNode* t1=headA;
+        ListNode* t2=headB;
+        int l1=0,l2=0,d;
+
+        while(t1!=NULL){
+            l1++;
+            t1=t1->next;
+        }
+        while(t2!=NULL){
+            l2++;
+            t2=t2->next;
+        }
+        d=abs(l1-l2);
+        t1=headA;
+        t2=headB;
+        if(l1>l2){
+            return collisionPoint(t1,t2,d);
+        }
+        else{
+            return collisionPoint(t2,t1,d);
+        }
+        return nullptr;*/
+
+        ListNode* t1=headA;
+        ListNode* t2=headB;
+
+        if(headA==NULL||headB==NULL){return NULL;}
+
+        while(t1!=t2){
+            t1=t1->next;
+            t2=t2->next;
+            if(t1==t2){return t1;}
+            if(t1==NULL){t1=headB;}
+            if(t2==NULL){t2=headA;}
+        }
+        return t1;
     }
 };
